be5f26ea23fcfcd0240ed820f429a935834a56df
andres857 <andres857@gmail.com>
2021-11-30 10:08:20 -0500

    fix minnor to susciber with client

--- broker/topics.js
+++ broker/topics.js
@@ -7,14 +7,14 @@ async function buildTopics(){
     let id = await serialPlayer()
     return {
         suscriber:{
-          streaming:`${client}player/streaming`,
-          restart:`${client}player/restart`,
-          request:`${client}player/request/${id}`,
+          streaming:`${client}/player/streaming`,
+          restart:`${client}/player/restart`,
+          request:`${client}/player/request/${id}`,
         },
         publish:{
-          status: `${client}player/status/${id}`,
-          response: `${client}player/response/${id}`,
-          currentStreaming: `${client}player/currentstreaming/${id}`
+          status: `${client}/player/status/${id}`,
+          response: `${client}/player/response/${id}`,
+          currentStreaming: `${client}/player/currentstreaming/${id}`
         }
     }
 }
