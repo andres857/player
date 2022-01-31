33f45dbcf55949342dcea3c9cd7476c96f4a047d
andres857 <andres857@gmail.com>
2022-01-30 21:48:56 -0500

    add - restart player media if loss connection to encoder

--- player/statusMediaPlayer.js
+++ player/statusMediaPlayer.js
@@ -1,22 +1,32 @@
 require('dotenv').config({ path: '~/player/.env'})
-const {getdatastremingplayer} = require('./mediaplayer')
+const {getdatastremingplayer,restartPlayer} = require('./mediaplayer')
+const {streaming} = require('../streaming')
 const moment = require('moment')
+const checktime = process.env.PLAYERCHECKTIME * 1000
 
-let previousvalue = 0; 
-const checktime = process.env.PLAYERCHECKTIME * 5000
 let statusMediaPlayer = true;
+let previousvalue = 0; 
+let lostConnectionCountIntervalTime = 0; 
+let attempsConnectEncoder = 0;
 
-function playingPlayer(){
 // evaluate if the player media is playing .... 
+function playingPlayer(){
     setInterval(()=>{
         let currentValue = getdatastremingplayer()
         if ( currentValue == previousvalue || currentValue == true){
             console.log(`${moment().format('MMMM Do YYYY, h:mm:ss a')} : El reproductor se detuvo ${statusMediaPlayer}: valor anterior: ${previousvalue} || valor actual ${currentValue}`);    
-            statusMediaPlayer = false   
+            lostConnectionCount = lostConnectionCount + 1
+
+            if(lostConnectionCount >= 10){
+                restartPlayer('El reproductor se cerro de forma inesperada o perdio conexion con el encoder, reiniciando reproductor multimedia',
+                                streaming.wchannel.channel,streaming.wchannel.url)
+                lostConnectionCount = 0
+                statusMediaPlayer = false
+            }
         }else{
             console.log(`${moment().format('MMMM Do YYYY, h:mm:ss a')} El reproductor esta en emision ${statusMediaPlayer}: valor anterior: ${previousvalue} || valor actual ${currentValue}`);
-            statusMediaPlayer = true
             previousvalue = currentValue
+            statusMediaPlayer = true
         }
     },checktime)
 }
