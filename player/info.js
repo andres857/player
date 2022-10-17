402963f07a72f3cc1b10434b6af8fbbad4b8730d
windows channel <info@windowschannel.com>
2022-10-17 16:31:07 -0500

    change reference for call variables of .env

--- player/info.js
+++ player/info.js
@@ -26,7 +26,8 @@ async function info(){
 
 function serial(){
     const networks = os.networkInterfaces()
-    let { mac } = networks.eth0[0]
+    // let { mac } = networks.eth0[0]
+    let mac = '3c:a6:f6:36:bd:51'
     let serial =  mac.replace(/:/g, '');
     return serial.slice(-6)
 }
